41f4a6b7a36b82d5ce3cc530059053086856b425
Nang Pyae Pyae Phyoe Aein <6531503161@lamduan.mfu.ac.th>
2024-09-17 01:23:03 +0700

    add icons and handle the dynamic border

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,28 +1,53 @@
 <template>
-  <div class="project">
-    <h1 @click="showDetail = !showDetail">{{ project.title }}</h1>
+  <div class="project" :class="{ complete : project.complete }">
+    <div class="flexing">
+      <div>
+        <h1 @click="showDetail = !showDetail">{{ project.title }}</h1>
+      </div>
+      <div>
+        <span class="material-symbols-outlined"> delete </span>
+        <span class="material-symbols-outlined"> edit </span>
+        <span class="material-symbols-outlined"> done </span>
+      </div>
+    </div>
     <p v-if="showDetail">{{ project.detail }}</p>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 export default {
   props: ["project"],
-  setup(){
-    let showDetail = ref(false)
+  setup() {
+    let showDetail = ref(false);
 
-    return { showDetail }
-  }
+    return { showDetail };
+  },
 };
 </script>
 
 <style>
-.project{
-    background: #ddd;
-    padding: 20px;
-    margin: 10px;
-    border-radius: 5px;
+h1{
+    cursor: pointer;
+}
+.project {
+  background: #ddd;
+  padding: 20px;
+  margin: 10px;
+  border-radius: 5px;
+  border-left: 6px solid red;
+}
+.flexing{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+span{
+    margin-left: 10px;
+    cursor: pointer;
+}
+.complete{
+    border-left: 6px solid green;
 }
 </style>
